1446cf9c7ae6f6ee8ac0d9c98129051dc8748637
McConderEz <kamazitun.jaykehd@gmail.com>
2023-07-18 01:07:44 +0300

    Implemented the Neuron class

---
+++ CodeBlogMachineLearning1/Neuron.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogMachineLearning1
+{
+    internal class Neuron
+    {
+        public List<double> Weights { get; }
+        public NeuronType NeuronType { get; }
+        public double Output { get; private set; }
+
+        public Neuron(int inputCount, NeuronType type = NeuronType.Normal)
+        {
+            if(inputCount == 0)
+            {
+                throw new ArgumentException("Количество входных нейронов не может равняться нулю!");
+            }
+
+            NeuronType = type;
+            Weights = new List<double>();
+
+            for(int i = 0;i < inputCount; i++)
+            {
+                Weights.Add(1);
+            }
+        }
+
+        public double FeedForward(List<double> inputs)
+        {
+            var sum = 0.0;
+            for(int i = 0;i < inputs.Count;i++)
+            {
+                sum += inputs[i] * Weights[i];
+            }
+
+            Output = Sigmoid(sum);
+            return Output;
+        }
+
+        private double Sigmoid(double x)
+        {
+            var result = 1.0 / (1.0 + Math.Pow(Math.E, -x));
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return Output.ToString();
+        }
+    }
+}
---
+++ CodeBlogMachineLearning1/NeuronType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlogMachineLearning1
+{
+    public enum NeuronType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+}
